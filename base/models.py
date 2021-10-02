a990d4cc8988b55eff51ec2394c5bf600266e1f5
lucas felici <lugas_felici@gmail.com>
2021-10-02 19:18:18 -0300

    pagina basica}

---
+++ base/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#db
+class Task(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    titulo= models.CharField(max_length=200, null=True, blank=True)
+    descripcion= models.TextField(null=True, blank=True)
+    completado= models.BooleanField(default=False)
+    creado= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        ordering = ['completado']
